c10a1ae49c22c3abc7145800129b76e3c7ddb48d
liangxp <liangxp0@126.com>
2019-12-10 22:47:40 +0800

    添加配置中心和Eureka注册中心

---
+++ itoken-config/src/main/java/ConfigApp.java
@@ -0,0 +1,6 @@
+/**
+ * @author Liangxp
+ * @date 2019/12/10 17:30
+ */
+public class ConfigApp {
+}
---
+++ itoken-eureka/src/mian/java/com/liang/EurekaApp.java
@@ -0,0 +1,8 @@
+package com.liang;
+
+/**
+ * @author Liangxp
+ * @date 2019/12/10 17:33
+ */
+public class EurekaApp {
+}

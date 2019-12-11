ebb3d90f463675c567d29af1da6926b9aed5bd41
liangxp <liangxp0@126.com>
2019-12-11 15:54:17 +0800

    重新添加分布式配置中心、Eureka注册中心、配置仓库

---
+++ itoken-config/src/main/java/com/liang/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 分布式配置中心服务端
+ * @author Liangxp
+ * @date 2019/12/11 15:05
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ itoken-eureka/src/main/java/com/liang/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka注册中心服务端
+ * @author Liangxp
+ * @date 2019/12/11 14:58
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}

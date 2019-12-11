5a504594dfa1bbc5cab18319a3dfc7df72a4f8f6
liangxp <liangxp0@126.com>
2019-12-11 10:30:06 +0800

    删除无效的并重新提交

--- itoken-config/src/main/java/ConfigApp.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Liangxp
- * @date 2019/12/10 17:30
- */
-public class ConfigApp {
-}
--- itoken-eureka/src/mian/java/com/liang/EurekaApp.java
+++
@@ -1,8 +0,0 @@
-package com.liang;
-
-/**
- * @author Liangxp
- * @date 2019/12/10 17:33
- */
-public class EurekaApp {
-}

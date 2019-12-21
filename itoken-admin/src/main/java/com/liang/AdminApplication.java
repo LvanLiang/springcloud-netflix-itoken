0128ecb7357567286329a13518a95e4050ae86ec
liangxp <liangxp0@126.com>
2019-12-21 23:03:34 +0800

    add

---
+++ itoken-admin/src/main/java/com/liang/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.liang;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liangxp
+ * @date 2019/12/12 15:47
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ itoken-zuul/src/main/java/com/liang/ZuulApplication.java
@@ -0,0 +1,17 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Liangxp
+ * @date 2019/12/12 15:53
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
